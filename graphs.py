12d5c86c5f8dbc74352cf69f56847164d987ca2a
pantari76 <59593062+pantari76@users.noreply.github.com>
2022-08-03 16:26:58 -0400

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -2,72 +2,90 @@ import sqlite3 as sql
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# A class that allows the user to make graphs easier
+class Graphs:
+    def __init__(self, cursor):
+        self.cursor = cursor
 
-def five_year_trend_graph(cursor):
-    # Creates a line graph of the changes in the 5 year trend of the Sea Otters population
-    # throughout the 29 years of surveying
+    # A function to create a line graph from two columns in the the database
+    def create_line_graph_two_col(self, query, x, y, label, title):
+        # fetching data from the SQL database
+        df = pd.DataFrame(data=self.cursor.execute(query).fetchall(), columns=[y, x])
 
-    # Creating an SQL query to get the data for the 5 Year Trend and id from the database
-    select_data = "SELECT trend5yr, id FROM otter;"
+        # Sizing the graph
+        plt.figure(figsize=(15, 4))
 
-    # Creating a Pandas DataFrame that contains the data
-    df = pd.DataFrame(data=cursor.execute(select_data).fetchall(), columns=["trend5yr", "id"])
+        # Plotting the graph
+        plt.plot(df[x], df[y], color="red", label=label)
 
-    # Setting the x and y value for plotting the graph
-    x = df["id"]
-    y = df["trend5yr"]
+        # Creating a legend
+        plt.legend()
 
-    # Sizing the graph
-    plt.figure(figsize=(15, 4))
+        # Labelling
+        plt.xlabel(x)
+        plt.ylabel(y)
+        plt.title(title)
 
-    # Plotting the graph
-    plt.plot(x, y, color="red", label="Trend 5 Year")
+        # Displaying
+        plt.show()
 
-    # Creating a legend
-    plt.legend()
+    # A function to create a graph comparing two queries (such as comparing the Pup Ratio between
+    # the Sea Otters in Zone N and the Sea Otters in Zone O
+    def compare_two_datasets_with_query(self, query1, query2, x, y, label1, label2, title):
 
-    # Labelling
-    plt.xlabel("Otter Data ID")
-    plt.ylabel("Rate of Change of Otters per Square km of Habitat")
-    plt.title("Changes in the 5 Year Trend for Sea Otters in California")
+        # Executing the queries
+        data1 = self.cursor.execute(query1).fetchall()
+        data2 = self.cursor.execute(query2).fetchall()
 
-    # Displaying
-    plt.show()
+        self.compare_two_datasets(data1, data2, x, y, label1, label2, title)
 
+    # A helper function that creates the plot
+    def compare_two_datasets(self, ds1, ds2, x, y, label1, label2, title):
+        # Sizing the graph
+        plt.figure(figsize=(14, 4))
 
-def zone_n_v_o(cursor):
-    # The function creates a graph comparing the Pup Ratio between the Sea Otters in Zone N
-    # and the Sea Otters in Zone O
+        # Plotting the graph
+        plt.plot(ds1, color="red", label=label1)
+        plt.plot(ds2, color="blue", label=label2)
 
-    # Creating SQL queries to get the Pup Ratio from Zone N and Zone O
-    select_zone_n = "SELECT pupratio FROM otter WHERE zonecode=\"n\";"
-    select_zone_o = "SELECT pupratio FROM otter WHERE zonecode=\"o\";"
+        # Creating a legend
+        plt.legend()
 
-    # Executing the queries
-    pupratio_n = cursor.execute(select_zone_n).fetchall()
-    pupratio_o = cursor.execute(select_zone_o).fetchall()
+        # Labelling
+        plt.xlabel(x)
+        plt.ylabel(y)
+        plt.title(title)
 
-    # Sizing the graph
-    plt.figure(figsize=(14, 4))
+        # Displaying
+        plt.show()
 
-    # Plotting the graph
-    plt.plot(pupratio_n, color="red", label="Zone N")
-    plt.plot(pupratio_o, color="blue", label="Zone O")
+    # A function that allows the user to input any two arrays and create a single line plot
+    def create_single_line_plot(self, ds1, ds2, x, y, label, title):
+        plt.plot(ds2, ds1, color="red", label=label)
 
-    # Creating a legend
-    plt.legend()
+        plt.xlabel(x)
+        plt.ylabel(y)
+        plt.title(title)
 
-    # Labelling
-    plt.xlabel("Year")
-    plt.ylabel("Relative Pup Abundance (Ratio of Pups to Independent Animals)")
-    plt.title("Pup Ratio per Otter ID in Zone N vs Zone O")
-
-    # Displaying
-    plt.show()
+        plt.show()
 
 
-def max_sea_otter_distribution_chart(cursor):
-    # Find the maximum sea otter density for each year and plots the data in a line graph
+def main():
+    # Creating a connection to the database
+    connection = sql.connect("otters.db")
+
+    # Creating a cursor object to execute SQL queries
+    cursor = connection.cursor()
+
+    # Executing the functions to create graphs from the data
+
+    graph = Graphs(cursor)
+    graph.create_line_graph_two_col("SELECT trend5yr, id FROM otter;", "id", "trend5yr", "Trend 5 Year",
+                            "Changes in the 5 Year Trend for Sea Otters in California")
+    graph.compare_two_datasets_with_query("SELECT pupratio FROM otter WHERE zonecode=\"n\";",
+                               "SELECT pupratio FROM otter WHERE zonecode=\"o\";",
+                               "Year", "Relative Pup Abundance (Ratio of Pups to Independent Animals)",
+                               "Zone N", "Zone O", "Pup Ratio per Otter ID in Zone N vs Zone O")
 
     # Creating two empty lists
     max_otter_dist = []
@@ -83,33 +101,12 @@ def max_sea_otter_distribution_chart(cursor):
     for i in range(1985, 2014):
         year.append(i)
 
-    # Plotting the graph
-    plt.plot(year, max_otter_dist, color="red", label="Max Sea Otter Distribution Per Year")
-
-    # Labelling
-    plt.xlabel("Otter ID")
-    plt.ylabel("Otters per Square km of Habitat")
-    plt.title("Maximum Sea Otter Density per Year")
-
-    # Displaying
-    plt.show()
-
-
-def main():
-    # Creating a connection to the database
-    connection = sql.connect("otters.db")
-
-    # Creating a cursor object to execute SQL queries
-    cursor = connection.cursor()
-
-    # Executing the functions to create graphs from the data
-    five_year_trend_graph(cursor)
-    max_sea_otter_distribution_chart(cursor)
-    zone_n_v_o(cursor)
-
+    graph.create_single_line_plot(max_otter_dist, year, "Otter ID", "Otters per Square km of Habitat",
+                                  "Max Sea Otter Distribution Per Year", "Maximum Sea Otter Density per Year")
     # Closing the connection
     connection.close()
 
 
+# Entry point
 if __name__ == "__main__":
     main()
